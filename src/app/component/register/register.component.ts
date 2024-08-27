ec1b8f472cce5433af114c56795305fbcb8ce305
Cleg <ewihinnovation@gmail.com>
2024-08-27 14:05:18 +0700

    auth-firebase

---
+++ src/app/component/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(private auth: AuthService){}
+
+  ngOnInit(): void {
+    
+  }
+
+  register(){
+    if(this.email == ''){
+      alert('Please enter email');
+      return;
+    }
+    if(this.password == ''){
+      alert('Please enter password');
+      return;
+    }
+
+    this.auth.register(this.email, this.password);
+    this.email='';
+    this.password='';
+  }
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+    apiKey: "AIzaSyDLW7nZrbuwK_19gYkW3u96u1JdHDuY8_o",
+    authDomain: "login-angular-firebase-e8038.firebaseapp.com",
+    projectId: "login-angular-firebase-e8038",
+    storageBucket: "login-angular-firebase-e8038.appspot.com",
+    messagingSenderId: "563810553716",
+    appId: "1:563810553716:web:dd476b913b18dbdf4e6621",
+    measurementId: "G-4VJMY5CNV8"
+    }
+};
